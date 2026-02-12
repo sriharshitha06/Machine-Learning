bfeaf493454469293af5ed8c52edf3ff7b427457
MANTHENA SRI HARSHITHA <manthenasiri23@gmail.com>
2026-02-12 22:41:29 +0530

    Add files via upload

---
+++ list nd tupple into array.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+# Convert a list to an array
+my_list = [1, 2, 3, 4, 5]
+array_from_list = np.array(my_list)
+print("Array from list:", array_from_list)
+
+# Convert a tuple to an array
+my_tuple = (6, 7, 8, 9, 10)
+array_from_tuple = np.array(my_tuple)
+print("Array from tuple:", array_from_tuple)
